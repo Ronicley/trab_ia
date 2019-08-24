20e353ddc3ca91807508e7ce634f8673407f5013
pablohenriques <41550392+pablohenriques@users.noreply.github.com>
2019-08-23 21:37:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -38,9 +38,65 @@ def convertBinaPraReal(valor):
     final = int(part1)+float(part2)
     return final
 
+##
+def modulo_abs(a):
+    resultado = abs(2 - (a*a))
+    return resultado
+##
+def lista_string(palavra):
+    lista_final = [] 
+    for i in range(len(palavra)):
+        num_aleatorio = randint(1,100)
+        sub_lista = [palavra[i], num_aleatorio]
+        lista_final.append(sub_lista)
+        lista_final.sort(key=lambda x: x[1])
+        num_aleatorio = 0
+    return lista_final
+##
+def lista_inverte_binario(l1, l2):
+    l3 = []
+    l4 = []
+
+    for i in range(8):
+        if i < 2:
+            l3.append(l1[i])
+            l4.append(l2[i])
+        elif i < 7:
+            l3.append(l2[i])
+            l4.append(l1[i])
+        else:
+            l3.append(l1[i])
+            l4.append(l2[i])
+    return l3, l4
+
+def lista_string(palavra):
+    lista_final = []
+    num_aleatorio = randint(1,100)
+    
+    for i in range(len(palavra)):
+        sub_lista = [palavra[i], num_aleatorio]
+        lista_final.append(sub_lista)
+
+    return lista_final
+
+
+num = float(input("Digite um nĂºmero:"))
 print(convertBinaPraReal('11111111'))
 #print(strNum('ronicley'))
 #print(troca('00000000'))
 #print(trocaTroca('00001111', '11110000'))
 #print(mod(-2))
 #print(criaListaCemStrings())
+##print(modulo_abs(num))
+##palavra = input("Digite algo:")
+##resultado = lista_string(palavra)
+##print(resultado)
+##for i in range(10):
+##    print(resultado[i])
+##l1 = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8]
+##l2 = [1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8]
+##print("Lista 01:", l1)
+##print("Lista 02:", l2)
+##print(lista_inverte_binario(l1, l2))
+##palavra = input("Digite algo:")
+##print(lista_string(palavra))
